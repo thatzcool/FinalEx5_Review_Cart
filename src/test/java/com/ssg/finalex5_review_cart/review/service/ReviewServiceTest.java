38a1a3384dfbc9e35307a72d7908d3485acfb271
thatzcool <mycaptain0771@gmail.com>
2024-11-14 15:52:56 +0900

    ReviewServiceTest_add

--- src/test/java/com/ssg/finalex5_review_cart/review/service/ReviewServiceTest.java
+++ src/test/java/com/ssg/finalex5_review_cart/review/service/ReviewServiceTest.java
@@ -1,7 +1,33 @@
 package com.ssg.finalex5_review_cart.review.service;
 
+import com.ssg.finalex5_review_cart.review.dto.ReviewDTO;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@Log4j2
 class ReviewServiceTest {
 
+    @Autowired
+    private ReviewService reviewService;
+
+    @Test
+    public void testRegister(){
+        Long pno = 100L;
+
+        ReviewDTO reviewDTO = ReviewDTO.builder().reviewText("첫번째 리뷰").score(5).reviewer("ssgSYM").pno(pno).build();
+
+        reviewService.register(reviewDTO);
+    }
+
+     @Test
+    public void testRead(){
+        Long rno = 100L;
+        ReviewDTO reviewDTO = reviewService.read(rno);
+        log.info(reviewDTO);
+     }
 }
